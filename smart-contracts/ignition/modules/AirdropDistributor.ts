fc654ecf8987538c45171e1992c2816161070ec8
Azeez Mumeen <azeezmumeen6@gmail.com>
2026-07-05 13:52:49 +0100

    switch root for envio deployment

--- smart-contracts/ignition/modules/AirdropDistributor.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("AirdropDistributorModule", (m) => {
-  const airdropDistributor = m.contract("Distributor");
-
-  return { airdropDistributor };
-});
--- smart-contracts/ignition/modules/LockFactory.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("LockFactoryModule", (m) => {
-  const lockFactory = m.contract("LockFactory");
-
-  return { lockFactory };
-});
